bc01e635efc347aef9e870e9be7746e4c72a46ae
Q-Mick <quentinmick@protonmail.com>
2023-05-08 18:11:16 -0600

    init

---
+++ app/App.js
@@ -0,0 +1,11 @@
+// import { ValuesController } from "./Controllers/ValuesController.js";
+import { PlayersController } from "./Controllers/PlayersControllers.js";
+class App {
+  // valuesController = new ValuesController();
+  playersController = new PlayersController();
+  // Create the players controller class
+  
+}
+
+
+window["app"] = new App();
